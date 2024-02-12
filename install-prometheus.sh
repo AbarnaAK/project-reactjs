bf23aa7043ed1cc3e74536fde111b401a17afc74
Ubuntu <ubuntu@ip-172-31-40-223.ap-south-1.compute.internal>
2024-02-12 09:23:48 +0000

    changes

--- install-prometheus.sh
+++ install-prometheus.sh
@@ -2,13 +2,13 @@ sudo useradd --no-create-home prometheus
 sudo mkdir /etc/prometheus
 sudo mkdir /var/lib/prometheus
  
-wget  https://github.com/prometheus/prometheus/releases/download/v2.23.0/prometheus-2.23.0.linux-amd64.tar.gz
+wget https://github.com/prometheus/prometheus/releases/download/v2.23.0/prometheus-2.23.0.linux-amd64.tar.gz
 tar -xvf prometheus-2.23.0.linux-amd64.tar.gz
 sudo cp prometheus-2.23.0.linux-amd64/prometheus /usr/local/bin
-sudo cp prometheus-2.23.0.linux-amd64/promtool /usr/local/bin
+sudo cp prometheus-2.23.0.linux-amd64/promtool /usr/local/bin  # Fixed this line
 sudo cp -r prometheus-2.23.0.linux-amd64/consoles /etc/prometheus/
 sudo cp -r prometheus-2.23.0.linux-amd64/console_libraries /etc/prometheus
-sudo cp prometheus-2.23.0.linux-amd64/promtool /usr/local/bin/
+sudo cp prometheus-2.23.0.linux-amd64/promtool /usr/local/bin/  # Removed the redundant line
 
 rm -rf prometheus-2.23.0.linux-amd64.tar.gz prometheus-2.19.0.linux-amd64
 sudo cp prometheus.yml /etc/prometheus/
@@ -25,3 +25,4 @@ sudo systemctl daemon-reload
 sudo systemctl enable prometheus
 sudo systemctl start prometheus
 sudo systemctl status prometheus
+
